998092564ee275326681368dd03be0baa651c573
Your Name <akalimol@e1r4p19.clusters.42paris.fr>
2022-12-25 19:56:24 +0100

    Commented version

--- get_next_line/get_next_line_bonus.c
+++ get_next_line/get_next_line_bonus.c
@@ -6,7 +6,7 @@
 /*   By: akalimol <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/25 15:44:49 by akalimol          #+#    #+#             */
-/*   Updated: 2022/12/25 16:05:56 by akalimol         ###   ########.fr       */
+/*   Updated: 2022/12/25 18:44:30 by akalimol         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,12 +58,26 @@ static char	*ft_create_line(char **_buffer)
 	return (line);
 }
 
+/*
+ * Similar to malloc, but super :)
+ * If buffer doesn't have enough space, then allocates memory X2 more
+ *
+ * Input:
+ * 	_buffer - an adress of a pointer 
+ * 	size - an address of actual size of allocated memory to buffer
+ * 	len - an address of the length of buffer filled with chars
+ *
+ * Reaturn:
+ * 	1 if succedded to allocate
+ * 	0 if failed
+ * */
+
 static int	ft_super_malloc(char **_buffer, int *size, int *len)
 {
-	char	*new_buffer;
+	char	*new_buffer; // pointer to new memory block
 	int		i;
 
-	if (*len + BUFFER_SIZE < *size)
+	if (*len + BUFFER_SIZE < *size) // Check if buffer has enough place for the next reading 
 		return (*len);
 	while (*size < BUFFER_SIZE)
 		*size = *size * 2 + 1;
@@ -83,25 +97,42 @@ static int	ft_super_malloc(char **_buffer, int *size, int *len)
 	return (1);
 }
 
+/*
+ * Read the line until it reaches the \n or \0
+ * 
+ * input:
+ * 	con - an address of a pointer of fd'th container
+ * 	fd - file descriptor
+ * 
+ * Returns:
+ *	1 if succedded to read and copy
+ *	0 if failed
+ * */
 static int	ft_read_line(char **con, int fd)
 {
-	char	*buffer;
-	int		bytes;
-	int		ind;
-	int		actual_size;
+	char	*buffer; // To save my readed bytes until it reaches new line of EOF
+	int		bytes; // Readed num  of bytes
+	int		ind; // The index of zero byte in "buffer"
+	int		actual_size; // The current size of "buffer"
 
 	actual_size = BUFFER_SIZE;
 	buffer = *con;
 	bytes = 0;
 	ind = ft_strlen(buffer);
-	while (ft_check(buffer + ind) == 0)
+	while (ft_check(buffer + ind) == 0) // "Buffer + ind" because I want the program to check only newly appended buffer. Not all 
 	{
 		ind = ind + bytes;
-		if (ft_super_malloc(&buffer, &actual_size, &ind) == 0)
-			return (ft_free_return(buffer));
-		bytes = read(fd, buffer + ind, BUFFER_SIZE);
+		if (ft_super_malloc(&buffer, &actual_size, &ind) == 0) // Supper malloc allocates x2 memory
+		{
+			free (buffer);	
+			return ('\0');
+		}
+		bytes = read(fd, buffer + ind, BUFFER_SIZE); // "Buffer + ind" copies the new bytes at the end of buffer
 		if (bytes < 0)
-			return (ft_free_return(buffer));
+		{
+			free (buffer);
+			return (return);
+		}
 		buffer[ind + bytes] = '\0';
 		if (bytes < BUFFER_SIZE)
 			break ;
@@ -112,18 +143,27 @@ static int	ft_read_line(char **con, int fd)
 
 char	*get_next_line(int fd)
 {
-	static char	*containers[1024];
-	char		*returner;
+	static char	*containers[1024]; // To contain all my buffers
+	char		*returner; // a string to return as a line
 
 	if (fd < 0 || fd > 1024 || BUFFER_SIZE < 1)
 		return ('\0');
+	
+	// In order to be time efficient, only needed containers are initialized
 	if (!containers[fd])
 	{
 		containers[fd] = (char *)malloc(1);
 		containers[fd][0] = '\0';
 	}
-	if (!ft_check(containers[fd]) && !ft_read_line(&containers[fd], fd))
-		return ('\0');
+	// Check for existence of new line in container
+	if (!ft_check(containers[fd]))
+	{
+		// read the file and return 1 if reading succeeded
+		// Save readed bytes to container
+		if (ft_read_line(&containers[fd], fd) == 0)
+			return ('\0');
+	}
+	// Create the line from container and delete that line from container
 	returner = ft_create_line(&containers[fd]);
 	if (!returner)
 		return ('\0');
